705385a8d859cc970a5bc1cd5fa749e2f9c44ecd
AurelienNoro <aurelien.noro@gmail.com>
2026-01-15 14:45:46 +0100

    V0.1

---
+++ Order(ex2).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace POOsuite
+{
+    internal class Order
+    {
+        //Attributs et Proprietes
+
+        public string CustomerName { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsCancelled { get; private set; }
+        public static int CreatedOrdersCount { get; private set; } //NBR Total de commandes créées
+        public static int CancelledOrdersCount { get; private set; } //NBR Total de commandes annulées
+        public static decimal TotalAmountCreated { get; private set; } //Somme des montants de toutes les commandes créées
+
+        //Constructeur
+        public Order(string customerName, decimal amount)
+        {
+            CustomerName = customerName;
+            Amount = amount;
+            IsCancelled = false;
+            CreatedOrdersCount++;
+            TotalAmountCreated += amount;
+        }
+
+        //Méthode
+        public void Cancel()
+        {
+            {
+                IsCancelled = true;
+                CancelledOrdersCount++;
+                TotalAmountCreated -= Amount;
+            }
+        }
+        public void DisplayOrderInfo()
+        {
+            Console.WriteLine($"Client: {CustomerName}, Montant: {Amount} $, Annulée: {IsCancelled}");
+        }
+    }
+}
--- obj/Debug/net10.0/POOsuite.AssemblyInfo.cs
+++ obj/Debug/net10.0/POOsuite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POOsuite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b698f44fab6af8f11d0ff0bd83e93ae06392c550")]
 [assembly: System.Reflection.AssemblyProductAttribute("POOsuite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POOsuite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
