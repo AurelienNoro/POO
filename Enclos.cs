cf56fc3ac164ae56050c48045b5b87ecfc244c9c
AurelienNoro <aurelien.noro@gmail.com>
2026-01-15 11:26:45 +0100

    Commit POO

---
+++ Enclos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POOsuite
+{
+    internal class Enclos
+    {
+        //Attributs
+        private string name;
+        private int capacity;
+        private List<Animal> animals;
+    
+
+        //Proprietes
+        public string Name { get { return name; } set { name = value; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public List<Animal> Animals { get { return animals; } set { animals = value; } }
+        //Constructeur
+        public Enclos(string name, int capacity)
+        {
+            this.name = name;
+            this.capacity = capacity;
+            this.animals = new List<Animal>();
+        }
+        //Methodes
+        public bool AddAnimal(Animal animal)
+        {
+            if (animals.Count < capacity)
+            {
+                animals.Add(animal);
+                Console.WriteLine($"{animal.Name} a été ajouté à l'enclos {name}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"L'enclos {name} est plein. Impossible d'ajouter {animal.Name}.");
+                return false;
+            }
+        }
+        public bool RemoveAnimal(Animal animal)
+        {
+            if (animals.Remove(animal))
+            {
+                Console.WriteLine($"{animal.Name} a été retiré de l'enclos {name}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{animal.Name} n'est pas dans l'enclos {name}.");
+                return false;
+            }
+        }
+        public void GetAnimalList()
+        {
+            Console.WriteLine($"Animaux dans l'enclos {name} :");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"- {animal.Name} ({animal.Species}), Énergie : {animal.Energy}");
+            }
+        }
+        public void PrintEnclosureStatus()
+        {
+            Console.WriteLine($"Enclos: {name}, Capacité: {capacity}, Nombre d'animaux: {animals.Count}");
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOsuite;
+
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.WriteLine("Mon parc animal");
+Console.WriteLine("-------------------------------------------------------------------------");
+
+
+//Enclos
+Enclos enclosNord = new Enclos("Enclos Nord", 2);
+
+//Animal
+Animal luna = new Animal("Luna", "Chat", 80);
+Animal rex = new Animal("Rex", "Chien", 50);
+Animal milo = new Animal("Milo", "Lapin", 30);
+
+//Affichage Animal
+Console.WriteLine("Informations des animaux:");
+Console.WriteLine("");
+luna.DisplayInfo();
+rex.DisplayInfo();
+milo.DisplayInfo();
+Console.WriteLine("-------------------------------------------------------------------------");
+
+//Ajouter des animaux à l'enclos
+Console.WriteLine("Ajout des animaux à l'enclos:");
+Console.WriteLine("");
+enclosNord.AddAnimal(luna);
+enclosNord.AddAnimal(rex);
+enclosNord.AddAnimal(milo);
+Console.WriteLine("-------------------------------------------------------------------------");
+
+//Appeler les methodes Eat et Play
+Console.WriteLine("Interaction avec les animaux:");
+Console.WriteLine("");
+luna.Play(40);
+rex.Eat(60);
+Console.WriteLine("-------------------------------------------------------------------------");
+
+//Affiche l'état de l'enclos
+Console.WriteLine("État de l'enclos:");
+Console.WriteLine("");
+enclosNord.PrintEnclosureStatus();
+Console.WriteLine("-------------------------------------------------------------------------");
+
+//Rapport vétérinaire
+VetoReport rapportVeto1 = new VetoReport("Luna", "Fatigue aprés jeu", 2);
+rapportVeto1.PrintReport();
+Console.WriteLine("");
+VetoReport rapportVeto2 = new VetoReport("Rex", "Trés bonne forme", 1);
+rapportVeto2.PrintReport();
+Console.WriteLine("");
+VetoReport rapportVeto3 = new VetoReport("Milo", "Probléme de digestion", 7);
+rapportVeto3.PrintReport();
+Console.WriteLine("-------------------------------------------------------------------------");
+
+//Retirer un animal de l'enclos
+Console.WriteLine("Retrait d'un animal de l'enclos:");
+Console.WriteLine("");
+enclosNord.RemoveAnimal(rex);
+enclosNord.GetAnimalList();
+Console.WriteLine("-------------------------------------------------------------------------");
+
